aa66ae587cc7ab43706babb6f4b4e5b745fd8856
Eason <leoling127@gmail.com>
2019-03-07 16:16:26 +0800

    edit text describe

--- test/fizzbuzz_spec.js
+++ test/fizzbuzz_spec.js
@@ -14,13 +14,13 @@ describe('test fizzbuzz function',function(){
 
     })
 
-    it('should return FizzBuzz when input divisibleBy 3',function(){
+    it('should return FizzBuzz when input divisibleBy 3 & 5',function(){
         let result=fizzBuzz(15)
         result.should.be.eql('FizzBuzz')
 
     })
 
-    it('should return input when input is not divisibleBy 3 & 5',function(){
+    it('should return input when input is not divisibleBy 3 or 5',function(){
         let result=fizzBuzz(11)
         result.should.be.eql(11)
 

18c5e918d6f9396c0b73a7a91fd73eb5d0621f10
Eason <leoling127@gmail.com>
2019-03-08 11:23:24 +0800

    edit fizzbuzz_spec.js

--- test/fizzbuzz_spec.js
+++ test/fizzbuzz_spec.js
@@ -1,29 +1,25 @@
-var should=chai.should()
+var should = chai.should()
 
-describe('test fizzbuzz function',function(){
+describe('test fizzbuzz function', function () {
 
-    it('should return Fizz when input divisibleBy 3',function(){
-        let result=fizzBuzz(9)
+    it('should return Fizz when input divisibleBy 3', function () {
+        let result = fizzBuzz(9)
         result.should.be.eql('Fizz')
-
     })
 
-    it('should return Buzz when input divisibleBy 3',function(){
-        let result=fizzBuzz(10)
+    it('should return Buzz when input divisibleBy 5', function () {
+        let result = fizzBuzz(10)
         result.should.be.eql('Buzz')
-
     })
 
-    it('should return FizzBuzz when input divisibleBy 3 & 5',function(){
-        let result=fizzBuzz(15)
+    it('should return FizzBuzz when input divisibleBy 3 & 5', function () {
+        let result = fizzBuzz(15)
         result.should.be.eql('FizzBuzz')
-
     })
 
-    it('should return input when input is not divisibleBy 3 or 5',function(){
-        let result=fizzBuzz(11)
+    it('should return input when input is not divisibleBy 3 or 5', function () {
+        let result = fizzBuzz(11)
         result.should.be.eql(11)
-
     })
-    
+
 })
